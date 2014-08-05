d6e5d9e66325854eead0c97704b8778dff77e81c
Justin Love <git@JustinLove.name>
2014-08-05 14:47:10 -0500

    planet surface area

--- ui/mods/surface_area/surface_area.js
+++ ui/mods/surface_area/surface_area.js
@@ -3,8 +3,12 @@
     var area = 0
     model.system().planets.forEach(function(planet) {
       area += 4 * Math.PI * Math.pow(planet.planet.radius, 2)
-      console.log(planet, planet.radius, area)
     })
+    return (area/1000).toFixed(0)
+  })
+
+  model.planetSurfaceArea = ko.computed(function() {
+    var area = 4 * Math.PI * Math.pow(model.radius(), 2)
     return (area/1000).toFixed(0)
   })
 
@@ -13,4 +17,7 @@
     $('#version_info').append($html)
     ko.applyBindings(model, $html.get(0));
   })
+
+  $('.div_sys_editor_group table tr:nth(3) td:first')
+    .append('<div><span data-bind="text: model.planetSurfaceArea"></span> km<sup>2</sup></div>')
 })()
