a01fb66005d916d97894852594506679faf3390b
Justin Love <git@JustinLove.name>
2014-08-05 15:07:25 -0500

    dynamic number formatting

--- ui/mods/surface_area/surface_area.js
+++ ui/mods/surface_area/surface_area.js
@@ -1,15 +1,33 @@
 (function() {
+  var formatedString = function (number) {
+      var formats = [{ postfix: '', divisor: 1 },
+                     { postfix: 'K', divisor: 1000 },
+                     { postfix: 'M', divisor: 1000000 },
+                     { postfix: 'G', divisor: 1000000000 },
+                     { postfix: 'T', divisor: 1000000000000 }];
+
+      number = Math.floor(number);
+      if (number === 0)
+          return '0';
+      var numDigits = String(Math.abs(number)).length;
+      var format = formats[Math.floor((numDigits - 1) / 3)];
+
+      number = format.postfix ? (number / format.divisor).toPrecision(3) : number = number / format.divisor
+
+      return number + format.postfix
+  };
+  
   model.systemSurfaceArea = ko.computed(function() {
     var area = 0
     model.system().planets.forEach(function(planet) {
       area += 4 * Math.PI * Math.pow(planet.planet.radius, 2)
     })
-    return (area/1000).toFixed(0)
+    return formatedString(area, false)
   })
 
   model.planetSurfaceArea = ko.computed(function() {
     var area = 4 * Math.PI * Math.pow(model.radius(), 2)
-    return (area/1000).toFixed(0)
+    return formatedString(area, false)
   })
 
   $.get('coui://ui/mods/surface_area/surface_area.html').then(function(html) {
@@ -19,5 +37,5 @@
   })
 
   $('.div_sys_editor_group table tr:nth(3) td:first')
-    .append('<div><span data-bind="text: model.planetSurfaceArea"></span> km<sup>2</sup></div>')
+    .append('<div><span data-bind="text: model.planetSurfaceArea"></span> m<sup>2</sup></div>')
 })()
